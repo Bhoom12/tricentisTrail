10fda4c43cbbc1d2914a3735b16c6f3436de9a60
Admin <Admin@DESKTOP-8PDR25K>
2023-01-25 13:00:22 +0530

    Added new Class by Chandana

---
+++ TricentisInsurance/src/testCasesOfTricentisAutomationModule/Trial_001.java
@@ -0,0 +1,10 @@
+package testCasesOfTricentisAutomationModule;
+
+import org.testng.annotations.Test;
+
+public class Trial_001 {
+	@Test
+	public void trail() {
+		System.out.println("hi bhoomika");
+	}
+}
