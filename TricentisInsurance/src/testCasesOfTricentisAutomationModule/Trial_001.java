d22d9d288e694ebe2333fce8a434ef30093cfb99
Admin <Admin@DESKTOP-8PDR25K>
2023-01-25 18:12:51 +0530

    Resolved

--- TricentisInsurance/src/testCasesOfTricentisAutomationModule/Trial_001.java
+++ TricentisInsurance/src/testCasesOfTricentisAutomationModule/Trial_001.java
@@ -1,7 +1,11 @@
 package testCasesOfTricentisAutomationModule;
 
 import org.testng.annotations.Test;
-
+/***
+ * 
+ * @author chandana
+ *
+ */
 public class Trial_001 {
 	@Test
 	public void trail() {
